f4ff461c2816cd823dd0d2d3e2584bbb766b5e3e
kiril.kirilov <kiril.kirilov.1985@abv.bg>
2015-02-27 15:05:01 +0200

    [rm16318] edx base development auth provider

---
+++ common/djangoapps/third_party_auth/edvera_backend.py
@@ -0,0 +1,67 @@
+from social.backends.oauth import BaseOAuth2
+
+from . import provider
+
+
+class EdveraProvider(provider.BaseProvider):
+    """Provider for EDvera's Oauth2 auth system."""
+
+    BACKEND_CLASS = EdveraOAuth2
+    ICON_CLASS = 'fa-google-plus'
+    NAME = 'Edvera'
+    SETTINGS = {
+        'SOCIAL_AUTH_EDVERA_OAUTH2_KEY': None,
+        'SOCIAL_AUTH_EDVERA_OAUTH2_SECRET': None,
+    }
+
+    @classmethod
+    def get_email(cls, provider_details):
+        return provider_details.get('email')
+
+    @classmethod
+    def get_name(cls, provider_details):
+        return provider_details.get('fullname')
+
+
+class EdveraOAuth2(BaseOAuth2):
+    """Edvera authentication backend"""
+
+    name = 'edvera-oauth2'
+
+    REDIRECT_STATE = False
+
+    SITE = 'http://wqu-agency.edvera.dev/'
+
+    AUTHORIZATION_URL = SITE + '/oauth/authorize'
+
+    ACCESS_TOKEN_URL = SITE + '/oauth/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+
+    REVOKE_TOKEN_URL = SITE + '/oauth/revoke'
+    REVOKE_TOKEN_METHOD = 'POST'
+
+    # The order of the default scope is important
+
+    DEFAULT_SCOPE = ['write', 'public', 'update']
+
+    def revoke_token_params(self, token, uid):
+        return {'token': token}
+
+    def revoke_token_headers(self, token, uid):
+        return {'Content-type': 'application/json'}
+
+    def get_user_id(self, details, response):
+        return response['user']['id']
+
+    def get_user_details(self, response):
+        user = response['user']
+        return {
+            'email': user['email'],
+            'fullname': user['first_name'] + ' ' + user['last_name'],
+            'firstname': user['first_name']
+        }
+
+    def user_data(self, access_token, *args, **kwargs):
+        return self.get_json(self.SITE + 'users/current', params={
+            'access_token': access_token
+        })
